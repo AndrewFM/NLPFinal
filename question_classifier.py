d427e8fce73bfb336dcbc7c5db7a17b6c29a5118
AndrewFM <AndrewFMs@gmail.com>
2015-04-15 16:17:58 -0400

    Switching from NLTK to Scikit Learn

--- question_classifier.py
+++ question_classifier.py
@@ -1,52 +1,36 @@
 #Takes a question as input, and soft-classifies it into the most likely stack-exchange sites in our corpus.
-
-import nltk
-from nltk.corpus import stopwords
 import os
-import string
-import pickle
+import sys
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import HashingVectorizer, TfidfTransformer
 
-question_limit = 500 #Number of training questions to use per file (since the full 2500 is wrecking the computer's memory)
-classifier = None
-table = str.maketrans("", "", string.punctuation) #Used to strip punctuation
+#Gather together the data
+data = []
+targets = []
+for site in os.listdir("Question Corpus"):
+	infile = open("Question Corpus/"+site, 'rb')
+	for line in infile:
+		data.append(line)
+		targets.append(site)
+	infile.close()
 
-#If we don't have a pre-trained classifier saved to disk, train one on the data.
-if os.path.isfile("SerializedClassifier.dat") == False:
-	#Construct Bag-of-words model for each website
-	train_set = [] 
+#Train the classifier
+#	HashingVectorizer = Bag of Words (used HashingVectorizer instead of CountVectorizer for memory conservation)
+#	TfidfTransformer  = Proportional word counts
+#	SGDClassifier	  = SVM w/ Stochastic gradient descent
+print("Building classifier...")
+classifier = Pipeline([('vect', HashingVectorizer(input='content', strip_accents='unicode', ngram_range=(1,2), stop_words='english')),		
+                      ('tfidf', TfidfTransformer()),								
+                      ('clf', SGDClassifier(n_jobs=-1))])	
+classifier.fit_transform(data, targets)								
 
-	for infile in os.listdir("Question Corpus"):
-	    site = open("Question Corpus/"+infile, 'r', encoding='utf-8')
-	    print("Compiling featureset for "+infile)
-	    
-	    words = set()
-	    words_so_far = 0
-	    for question in site:
-	    	words = words.union(set([word.lower() for word in question.translate(table).split(' ')]))
-	    	words_so_far += 1
-	    	if words_so_far > question_limit:
-	    		break
-	    site.close()
-
-	    train_set.append( (dict.fromkeys(words, True), infile) )
-
-	#Train the classifier
-	print("Building classifier...")
-	classifier = nltk.NaiveBayesClassifier.train(train_set)
-	p = pickle.Pickler(open("SerializedClassifier.dat", "wb"))
-	p.fast = True
-	p.dump(classifier)
-
-#Otherwise, load the classifier from disk.
-else:
-	print("Loading the classifier from file...")
-	classifier = pickle.load( open("SerializedClassifier.dat", "rb") )
+#(Todo): Gather together test data (some subset of the training data, not used during training), and output classification accuracy
 
 #Get questions
 while True:
     user_question = input("Enter a question: ")
-    word_arr = user_question.lower().translate(table).split(" ")
-    print("I think your question belongs to: "+classifier.classify(nltk.FreqDist(word_arr)))
+    print("I think your question belongs to: "+classifier.predict([user_question])[0])
 
 
 
