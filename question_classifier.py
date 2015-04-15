ff9a157e00ce43da367e333a81c38b048d101709
MoonWatcher582 <erbronne@gmail.com>
2015-04-14 21:35:50 -0400

    semi-accurate kinda sorta

--- question_classifier.py
+++ question_classifier.py
@@ -3,32 +3,51 @@
 import nltk
 import os
 import string
+from nltk.corpus import stopwords
+
+punctuation = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+
+def remove_punctuation(word):
+    stripped_str = ""
+    for c in word:
+        if c in punctuation:
+            continue
+        stripped_str += c
+    return stripped_str
 
 #Construct Bag-of-words model for each website
 word_counts = []
-ids = []
-maketrans = ''.maketrans
+ids = []   
 
 for infile in os.listdir("Question Corpus"):
-	site = open("Question Corpus/"+infile, 'r')
-	print("Question Corpus/"+infile)
-	words = []
-	for question in site:
-		for word in question.split(' '):
-			words.append(word.lower().translate(maketrans("",""), string.punctuation))
-	site.close()
+    site = open("Question Corpus/"+infile, 'r')
+    print("Question Corpus/"+infile)
+    words = []
+    questions_seen = 0
+    for question in site:
+        for word in question.split(' '):
+               word = remove_punctuation(word.lower())
+               if word not in stopwords.words('english'):
+                   words.append(word)
+        questions_seen += 1
+        if questions_seen > 500:
+            break
+    site.close()
 
-	word_counts.append(nltk.FreqDist(words))
-	ids.append(infile)
+    word_counts.append(nltk.FreqDist(set(words)))
+    ids.append(infile)
 
 #Train the classifier
-train_set = [(words_counts[i], ids[i]) for i in range(len(ids))]
-classifier = nltk.NaiveBayesClassifier(train_set)
+train_set = [(word_counts[i], ids[i]) for i in range(len(ids))]
+print("Building classifier...")
+classifier = nltk.NaiveBayesClassifier.train(train_set)
 
 #Get questions
 while True:
-	user_question = input("Enter a question: ")
-	print("I think your question belongs to: "+classifier.classify(user_question))
+    user_question = raw_input("Enter a question: ")
+    user_question = remove_punctuation(user_question.lower())
+    word_arr = user_question.split(" ")
+    print("I think your question belongs to: "+classifier.classify(nltk.FreqDist(word_arr)))
 
 
 
