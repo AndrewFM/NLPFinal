1e811eec7b96f6603cfd972c9b15c3cc47215b04
AndrewFM <AndrewFMs@gmail.com>
2015-04-15 00:50:26 -0400

    Serializing classifier to and from file

--- question_classifier.py
+++ question_classifier.py
@@ -1,52 +1,51 @@
 #Takes a question as input, and soft-classifies it into the most likely stack-exchange sites in our corpus.
 
 import nltk
+from nltk.corpus import stopwords
 import os
 import string
-from nltk.corpus import stopwords
+import pickle
 
-punctuation = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+question_limit = 500 #Number of training questions to use per file (since the full 2500 is wrecking the computer's memory)
+classifier = None
+table = str.maketrans("", "", string.punctuation) #Used to strip punctuation
 
-def remove_punctuation(word):
-    stripped_str = ""
-    for c in word:
-        if c in punctuation:
-            continue
-        stripped_str += c
-    return stripped_str
+#If we don't have a pre-trained classifier saved to disk, train one on the data.
+if os.path.isfile("SerializedClassifier.dat") == False:
+	#Construct Bag-of-words model for each website
+	train_set = [] 
 
-#Construct Bag-of-words model for each website
-word_counts = []
-ids = []   
+	for infile in os.listdir("Question Corpus"):
+	    site = open("Question Corpus/"+infile, 'r', encoding='utf-8')
+	    print("Compiling featureset for "+infile)
+	    
+	    words = set()
+	    words_so_far = 0
+	    for question in site:
+	    	words = words.union(set([word.lower() for word in question.translate(table).split(' ')]))
+	    	words_so_far += 1
+	    	if words_so_far > question_limit:
+	    		break
+	    site.close()
 
-for infile in os.listdir("Question Corpus"):
-    site = open("Question Corpus/"+infile, 'r')
-    print("Question Corpus/"+infile)
-    words = []
-    questions_seen = 0
-    for question in site:
-        for word in question.split(' '):
-               word = remove_punctuation(word.lower())
-               if word not in stopwords.words('english'):
-                   words.append(word)
-        questions_seen += 1
-        if questions_seen > 500:
-            break
-    site.close()
+	    train_set.append( (dict.fromkeys(words, True), infile) )
 
-    word_counts.append(nltk.FreqDist(set(words)))
-    ids.append(infile)
+	#Train the classifier
+	print("Building classifier...")
+	classifier = nltk.NaiveBayesClassifier.train(train_set)
+	p = pickle.Pickler(open("SerializedClassifier.dat", "wb"))
+	p.fast = True
+	p.dump(classifier)
 
-#Train the classifier
-train_set = [(word_counts[i], ids[i]) for i in range(len(ids))]
-print("Building classifier...")
-classifier = nltk.NaiveBayesClassifier.train(train_set)
+#Otherwise, load the classifier from disk.
+else:
+	print("Loading the classifier from file...")
+	classifier = pickle.load( open("SerializedClassifier.dat", "rb") )
 
 #Get questions
 while True:
-    user_question = raw_input("Enter a question: ")
-    user_question = remove_punctuation(user_question.lower())
-    word_arr = user_question.split(" ")
+    user_question = input("Enter a question: ")
+    word_arr = user_question.lower().translate(table).split(" ")
     print("I think your question belongs to: "+classifier.classify(nltk.FreqDist(word_arr)))
 
 
